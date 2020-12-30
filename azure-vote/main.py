f736437d38f73b9f45d04c3b1cd4e3581a636d46
roland <you@example.com>
2020-12-30 22:18:08 +0100

    log dogs and cats votes

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -100,11 +100,11 @@ def index():
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            logger.info(properties)
+            logger.info('log cats votes', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            logger.info(properties)
+            logger.info('log dogs votes', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
