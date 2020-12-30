f0892da2a247402cc21be04fa53f979ab3c79a5a
roland <you@example.com>
2020-12-30 22:09:42 +0100

    add handler to logger, change InstrumentationKey

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -23,15 +23,19 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 # Logging
 logger = logging.getLogger(__name__)
 
+logger.addHandler(AzureLogHandler(
+    connection_string='InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8')
+)
+
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
   enable_standard_metrics=True,
-  connection_string='InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/')
+  connection_string='InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8')
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string='InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/'),
+        connection_string='InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8'),
     sampler=ProbabilitySampler(1.0),
 )
 
@@ -40,7 +44,7 @@ app = Flask(__name__)
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string="InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/"),
+    exporter=AzureExporter(connection_string="InstrumentationKey=361f7b58-d0a0-4da8-a648-5b1559d163c8"),
     sampler=ProbabilitySampler(rate=1.0),
 )
 
